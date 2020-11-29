07cd4a7b5ce7014676b2d1c210b155af7b2de83c
Adriano Avelino <adriano-st777@hotmail.com>
2020-11-29 15:09:59 -0300

    adiciona teste de repository com outro banco de dados

--- src/test/java/br/com/alura/forum/repository/CursoRepositoryTest.java
+++ src/test/java/br/com/alura/forum/repository/CursoRepositoryTest.java
@@ -4,7 +4,11 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import br.com.alura.forum.modelo.Curso;
@@ -12,14 +16,24 @@ import br.com.alura.forum.modelo.Curso;
 
 @DataJpaTest
 @RunWith(SpringRunner.class)
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@ActiveProfiles("test")
 public class CursoRepositoryTest {
 
 	@Autowired
 	private CursoRepository repository;
 	
+	@Autowired
+	private TestEntityManager em;
+	
 	@Test
 	public void deverBuscarUmCursoPorNome() {
 		String nomeCurso = "HTML 5";
+		Curso newCurso = new Curso();
+		newCurso.setNome(nomeCurso);
+		newCurso.setCategoria("Programacao");
+		em.persist(newCurso);
+		
 		Curso curso = repository.findByNome(nomeCurso);
 		Assert.assertNotNull(curso);
 		Assert.assertEquals(nomeCurso, curso.getNome());
